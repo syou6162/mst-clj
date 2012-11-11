7a109626e1f913a86c0903f6b45bd516d97280a2
Yasuhisa Yoshida <syou6162@gmail.com>
2012-11-11 21:32:54 +0900

    Eisner法を追加

---
+++ src/mst_clj/eisner.clj
@@ -0,0 +1,78 @@
+(ns mst-clj.eisner
+  (:use [mst-clj.perceptron]))
+
+(defn argmax [coll]
+  "Return max val and its index"
+  (assert (not (empty? coll)))
+  (let [init-idx -1
+        init-val Double/NEGATIVE_INFINITY]
+    (reduce (fn [[max-idx max-val] [idx val]]
+              (if (> val max-val)
+                [idx val]
+                [max-idx max-val]))
+            [init-idx init-val]
+            coll)))
+
+(defn init-dp-table [n]
+  (make-array Double/TYPE n n 2 2))
+
+(defmacro deep-aget
+  ([hint array idx]
+     `(aget ~(vary-meta array assoc :tag hint) ~idx))
+  ([hint array idx & idxs]
+     `(let [a# (aget ~(vary-meta array assoc :tag 'objects) ~idx)]
+        (deep-aget ~hint a# ~@idxs))))
+
+(defmacro deep-aset [hint array & idxsv]
+  (let [hints '{doubles double ints int}
+        [v idx & sxdi] (reverse idxsv)
+        idxs (reverse sxdi)
+        v (if-let [h (hints hint)] (list h v) v)
+        nested-array (if (seq idxs)
+                       `(deep-aget ~'objects ~array ~@idxs)
+                       array)
+        a-sym (with-meta (gensym "a") {:tag hint})]
+    `(let [~a-sym ~nested-array]
+       (aset ~a-sym ~idx ~v))))
+
+(defn eisner [sentence weight]
+  (let [n (count sentence)
+        dp-table (init-dp-table n)
+        backtrack-pointer (atom {})
+        S (score-fn weight sentence)]
+    (doseq [m (range 1 n), s (range n), :let [t (+ s m)], :while (< t n)]
+      ;; incomplete-span
+      (let [[max-idx max-val] (argmax (map (fn [q] [q (+ (deep-aget doubles dp-table s q 1 0)
+                                                         (deep-aget doubles dp-table (inc q) t 0 0))])
+                                           (range s t)))
+            pointer (concat (get-in @backtrack-pointer [s max-idx 1 0] [])
+                            (get-in @backtrack-pointer [(inc max-idx) t 0 0] []))]
+        (deep-aset doubles dp-table s t 0 1 (+ (S t s) max-val))
+        (swap! backtrack-pointer assoc-in [s t 0 1] (conj pointer [t s])))
+      
+      (let [[max-idx max-val] (argmax (map (fn [q] [q (+ (deep-aget doubles dp-table s q 1 0)
+                                                         (deep-aget doubles dp-table (inc q) t 0 0))])
+                                           (range s t)))
+            pointer (concat (get-in @backtrack-pointer [s max-idx 1 0] [])
+                            (get-in @backtrack-pointer [(inc max-idx) t 0 0] []))]
+        (deep-aset doubles dp-table s t 1 1 (+ (S s t) max-val))
+        (swap! backtrack-pointer assoc-in [s t 1 1] (conj pointer [s t])))
+      ;; complete-span
+      (let [[max-idx max-val] (argmax (map (fn [q] [q (+ (deep-aget doubles dp-table s q 0 0)
+                                                         (deep-aget doubles dp-table q t 0 1))])
+                                           (range s t)))
+            pointer (concat (get-in @backtrack-pointer [s max-idx 0 0] [])
+                            (get-in @backtrack-pointer [max-idx t 0 1] []))]
+        (deep-aset doubles dp-table s t 0 0 max-val)
+        (swap! backtrack-pointer assoc-in [s t 0 0] pointer))
+      
+      (let [[max-idx max-val] (argmax (map (fn [q] [q (+ (deep-aget doubles dp-table s q 1 1)
+                                                         (deep-aget doubles dp-table q t 1 0))])
+                                           (range (inc s) (inc t))))
+            pointer (concat (get-in @backtrack-pointer [s max-idx 1 1] [])
+                            (get-in @backtrack-pointer [max-idx t 1 0] []))]
+        (deep-aset doubles dp-table s t 1 0 max-val)
+        (swap! backtrack-pointer assoc-in [s t 1 0] pointer)))
+    (reduce (fn [sent [head modifier]] (assoc-in sent [modifier :head] head))
+            sentence
+            (get-in @backtrack-pointer [0 (dec n) 1 0]))))
