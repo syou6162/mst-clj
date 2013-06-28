b8656b9e2e28b09cf0c83c3318bebbfaf89f2854
Yasuhisa Yoshida <syou6162@gmail.com>
2013-06-28 05:53:34 -0700

    パトリシア木から普通のhash-mapに戻した
    - look upが遅いので

--- src/mst_clj/mapping.clj
+++ src/mst_clj/mapping.clj
@@ -1,47 +1,28 @@
 (ns mst-clj.mapping
   (:use [clj-utils.io :only (serialize deserialize)]))
-;; Reference: http://concurrent-trees.googlecode.com/svn/concurrent-trees/javadoc/apidocs/com/googlecode/concurrenttrees/radix/ConcurrentRadixTree.html
-
-(import [com.googlecode.concurrenttrees.radix.node.concrete DefaultCharArrayNodeFactory])
-(import [com.googlecode.concurrenttrees.radix ConcurrentRadixTree])
 
 (defmacro def-obj-and-id-mapping [obj-name]
   "Define the functions related to converting object => id."
   (let [obj-to-id (symbol (str obj-name "-to-id"))
-        id-to-obj (symbol (str "id-to-" obj-name))
         save-obj-to-id (symbol (str "save-" obj-name "-to-id"))
         load-obj-to-id (symbol (str "load-" obj-name "-to-id"))
         clear-obj-mapping! (symbol (str "clear-" obj-name "-mapping!"))
-        get-max-obj-id (symbol (str "get-max-" obj-name "-id"))
-        set-max-obj-id! (symbol (str "set-max-" obj-name "-id!"))]
-    `(let [obj-to-id-mapping# (new ConcurrentRadixTree (new DefaultCharArrayNodeFactory))
-           max-obj-id# (atom -1)]
-       (defn ~get-max-obj-id [] @max-obj-id#)
-       (defn ~set-max-obj-id! [id#] (reset! max-obj-id# id#))
-       (defn ~obj-to-id [obj#]
-         (if-let [result# (.getValueForExactKey obj-to-id-mapping# obj#)]
-           result#
-           (let [max-id# (swap! max-obj-id# inc)]
-             (.put obj-to-id-mapping# obj# max-id#)
-             max-id#)))
-       (defn ~save-obj-to-id [filename#]
-         (binding [*out* (java.io.FileWriter. filename#)]
-           (println (~get-max-obj-id))
-           (doseq [p# (-> obj-to-id-mapping#
-                          (.getKeyValuePairsForKeysStartingWith  ""))]
-             (let [k# (.getKey p#)
-                   v# (.getValue p#)]
-               (println (str k# " " v#))))))
-       (defn ~load-obj-to-id [filename#]
-         (with-open [br# (-> (java.io.FileInputStream. filename#)
-                             (java.io.InputStreamReader.)
-                             (java.io.BufferedReader.))]
-           (~set-max-obj-id! (Integer/parseInt (. br# readLine)))
-           (doseq [line# (line-seq br#)]
-             (let [[k# v#] (clojure.string/split line# #" ")]
-               (.put obj-to-id-mapping# k# (Integer/parseInt v#))))))
-       (defn ~clear-obj-mapping! []
-         (~set-max-obj-id! -1)
-         (doseq [k# (-> obj-to-id-mapping#
-                        (.getKeysStartingWith ""))]
-           (.remove obj-to-id-mapping# k#))))))
+        obj-to-id-mapping (symbol (str obj-name "-to-id-mapping"))
+        max-obj-id (symbol (str "max-" obj-name "-id"))]
+    `(let [obj-to-id-mapping# (atom {})]
+       (do
+         (defn ~obj-to-id [obj#]
+           (if-let [v# (get @obj-to-id-mapping# obj#)]
+             v#
+             (let [max-id# (count @obj-to-id-mapping#)]
+               (swap! obj-to-id-mapping# assoc obj# max-id#)
+               max-id#)))
+         (defn ~save-obj-to-id [filename#]
+           (serialize @obj-to-id-mapping# filename#))
+         (defn ~load-obj-to-id [filename#]
+           (reset! obj-to-id-mapping# (deserialize filename#)))
+         (defn ~clear-obj-mapping! []
+           (reset! obj-to-id-mapping# {}))
+         (defn ~obj-to-id-mapping [] @obj-to-id-mapping#)
+         (defn ~max-obj-id []
+           (count @obj-to-id-mapping#))))))
