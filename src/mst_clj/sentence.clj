638e20ebccfa63ab09196f69c88c5273f5f84097
Yasuhisa Yoshida <syou6162@gmail.com>
2013-07-03 19:33:31 -0700

    training時とtest時で素性idの作り方を変えるように変更

--- src/mst_clj/sentence.clj
+++ src/mst_clj/sentence.clj
@@ -1,6 +1,6 @@
 (ns mst-clj.sentence
-  (:use [mst-clj.word])
-  (:use [mst-clj.feature]))
+  (:import [mst_clj.word Word])
+  (:use [mst-clj.feature :only (*update-feature-id?* get-fv)]))
 
 (deftype Sentence [words edge-fvs]
   clojure.lang.Seqable
@@ -16,13 +16,33 @@
   (assoc [this k v] (Sentence. (assoc (.words this) k v) (.edge-fvs this)))
   (valAt [this idx] (nth (.words this) idx nil)))
 
-(defn make [words]
+(defn make-training-data [words]
   (let [n (count words)
-        init-edge-fvs (vec (repeat n (vec (repeat n []))))
+        init-edge-fvs (vec (repeat n (vec (repeat n (int-array 0)))))
+        edge-fvs (->> (range 1 n)
+                      (reduce (fn [result i]
+                                (let [j (.head ^Word (nth words i))
+                                      fv (get-fv words i j)]
+                                  (assoc-in result [i j])))
+                              init-edge-fvs))]
+    (->> (for [i (range n), j (range 1 n)
+               :when (and (not= i j)
+                          (not (= j (.head ^Word (nth words i)))))]
+           [i j])
+         (reduce (fn [result [i j]]
+                   (binding [*update-feature-id?* false]
+                     (assoc-in result [i j] (get-fv words i j))))
+                 edge-fvs)
+         (Sentence. words))))
+
+(defn make-test-data [words]
+  (let [n (count words)
+        init-edge-fvs (vec (repeat n (vec (repeat n (int-array 0)))))
         edge-fvs (reduce (fn [result [i j]]
-                           (assoc-in result [i j] (get-fv words i j)))
+                           (binding [*update-feature-id?* false]
+                             (assoc-in result [i j] (get-fv words i j))))
                          init-edge-fvs
-                         (for [i (range n), j (range n)
+                         (for [i (range n), j (range 1 n)
                                :when (not= i j)]
                            [i j]))]
     (Sentence. words edge-fvs)))
