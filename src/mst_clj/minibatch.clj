c80b94033b2391e243fb8f781be7ceed46118bb5
Yasuhisa Yoshida <syou6162@gmail.com>
2013-09-20 04:03:53 -0700

    重みベクトルは諦めて破壊的に更新していくように変更

--- src/mst_clj/minibatch.clj
+++ src/mst_clj/minibatch.clj
@@ -1,39 +1,50 @@
 (ns mst-clj.minibatch
   (:use [mst-clj.io :only (my-pmap)])
   (:use [mst-clj.eisner :only (eisner-for-training)])
-  (:use [mst-clj.perceptron :only (fv-diff get-step-size update-weight!)])
+  (:use [mst-clj.perceptron
+         :only (get-fv-diff get-step-size update-weight! completely-correct?)])
+  (:use [mst-clj.evaluation :only (get-dependency-accuracy get-complete-accuracy)])
   (:import [mst_clj.word Word])
   (:import [mst_clj.sentence Sentence]))
 
 (def ^:dynamic *number-of-mini-batches* 24)
 (def ^:dynamic *number-of-threads* (min 4 *number-of-mini-batches*))
 
+(defn get-violated-set [^doubles weight mini-batch]
+  (let [result (->> mini-batch
+                    (my-pmap
+                     *number-of-threads*
+                     (fn [[idx gold]]
+                       (let [prediction (eisner-for-training gold weight)
+                             fv-diff (get-fv-diff gold prediction)]
+                         [idx gold prediction fv-diff]))))
+        predictions (mapv #(nth % 2) result)]
+    (-> (remove
+         (fn [[_ gold predict _]] (completely-correct? gold predict))
+         result)
+        (with-meta {:predictions predictions}))))
+
 (defn minibatch-update-weight
-  [iter ^doubles init-weight ^doubles init-cum-weight gold-sentences]
-  (let [n (count gold-sentences)
+  [iter ^doubles weight ^doubles cum-weight gold-sentences]
+  (let [predicts (atom [])
+        n (count gold-sentences)
         mini-batches (->> gold-sentences
                           (map-indexed #(vector (inc %1) %2))
                           (partition-all *number-of-mini-batches*)
                           (vec))]
-    (->> mini-batches
-         (reduce
-          (fn [[old-weight cum-old-weight] Dj]
-            (let [C (->> Dj
-                         (my-pmap
-                          *number-of-threads*
-                          (fn [[idx gold]]
-                            (let [prediction (eisner-for-training gold old-weight)]
-                              [idx gold prediction])))
-                         (remove
-                          (fn [[idx gold prediction]]
-                            (= (map #(:head ^Word %) (:words gold))
-                               (map #(:head ^Word %) (:words prediction))))))]
-              (doseq [[idx gold prediction] C]
-                (let [step-size (get-step-size old-weight gold prediction)
-                      diff (->> (fv-diff gold prediction)
-                                (map (fn [[k v]] [k (* step-size v)])))
-                      number-of-cum-examples (+ (* iter n) idx)]
-                  (update-weight! old-weight diff 1.0)
-                  (update-weight! cum-old-weight diff number-of-cum-examples)))
-              [old-weight cum-old-weight]))
-          [init-weight init-cum-weight]))))
+    (doseq [Dj mini-batches]
+      (let [C (get-violated-set weight Dj)]
+        (doseq [prediction (-> C meta :predictions)]
+          (swap! predicts conj prediction))
+        (doseq [[idx gold prediction fv-diff] C]
+          (let [step-size (get-step-size weight gold prediction fv-diff)
+                diff (->> fv-diff
+                          (map (fn [[k v]] [k (* step-size v)])))
+                number-of-cum-examples (+ (* iter n) idx)]
+            (update-weight! weight diff 1.0)
+            (update-weight! cum-weight diff number-of-cum-examples)))))
+    (->> [iter
+          (get-dependency-accuracy gold-sentences @predicts)
+          (get-complete-accuracy gold-sentences @predicts)]
+         (clojure.string/join ", ")
+       (println))))
