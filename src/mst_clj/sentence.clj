caaf2a9468f73602530b5b33211323d5863d14b8
Yasuhisa Yoshida <syou6162@gmail.com>
2013-09-23 16:46:11 -0700

    文内の単語間のエッジに張る素性にもラベルを考慮するように変更

--- src/mst_clj/sentence.clj
+++ src/mst_clj/sentence.clj
@@ -1,14 +1,20 @@
 (ns mst-clj.sentence
+  (:use [mst-clj.label :only (get-labeled-fv label-to-id-mapping)])
   (:use [mst-clj.feature :only (*update-feature-id?* get-fv)]))
 
 (defrecord Sentence [words edge-fvs])
 
 (defn make [words]
   (let [n (count words)
-        init-edge-fvs (vec (repeat n (vec (repeat n (int-array 0)))))
+        init-edge-fvs {}
         edge-fvs (reduce (fn [result [i j]]
                            (binding [*update-feature-id?* false]
-                             (assoc-in result [i j] (get-fv words i j))))
+                             (let [fv (get-fv words i j)]
+                               (->> (label-to-id-mapping)
+                                    (reduce
+                                     (fn [result [label id]]
+                                       (assoc-in result [i label j] (get-labeled-fv label fv)))
+                                     result)))))
                          init-edge-fvs
                          (for [i (range n), j (range n) :when (not= i j)]
                            [i j]))]
